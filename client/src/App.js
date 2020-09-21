75be625cea825d9e122ab731940add35ea3c22b9
Juyean Lee <juyeana@naver.com>
2020-09-21 16:57:13 -0700

    added components and setState

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Footer from './components/layout/Footer';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+
 
 function App() {
   return (
-    <div className='App'>
-    <h1>My App</h1>
-    </div>
+    <Router>
+      <div className='App'>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <Route exact path='/register' component={Register} />
+        <Route exact path='/login' component={Login} />
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function footer() {
   return (
-    <div>
-      <h3>footer</h3>
-    </div>
-  )
+    <footer className='bg-dark text-white mt-5 p-4 text-center'>
+      Copyright &copy; {new Date().getFullYear()} Dev Connector
+    </footer>
+  );
 }
