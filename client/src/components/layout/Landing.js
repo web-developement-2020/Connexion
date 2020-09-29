b2dc81abdc5db9150f1350b39cc257efe72b296c
Kanchan Sharma <58194538+Kakkoo@users.noreply.github.com>
2020-09-29 13:53:05 -0700

    Update Landing.js

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -23,6 +23,9 @@ class landing extends Component {
                 <Link to="/login" className="btn btn-lg btn-light bg-light" id="login-link">
                   Login
                 </Link>
+      <Link to="/forgotPassword" className="btn btn-lg btn-light">
+                  ForgotPassword
+                </Link>
                 </div>
               </div>
             </div>
