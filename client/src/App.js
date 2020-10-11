1903fa1d30043c43a9fe83d9fc289da7a006672c
Kakkoo <58194538+Kakkoo@users.noreply.github.com>
2020-10-11 12:11:57 -0700

    redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React, { Component } from "react";
 import { Provider } from "react-redux";
 import Navbar from "./components/layout/Navbar";
@@ -33,44 +33,4 @@ class App extends Component {
   }
 }
 export default App;
-=======
 
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import './App.css';
-import Navbar from './components/layout/Navbar';
-import Landing from './components/layout/Landing';
-import Footer from './components/layout/Footer';
-import Register from './components/auth/Register';
-import Login from './components/auth/Login';
-import CreatePost from './components/auth/CreatePost';
-import Post from './components/auth/Post';
-import Facebook from "./components/Facebook";
-import ForgotPassword from "./components/auth/ForgotPassword";
-
-import CreateProfile from './components/auth/CreateProfile';
-
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Navbar />
-
-        <Route exact path='/' component={Landing} />
-        <Route exact path='/register' component={Register} />
-        <Route exact path='/login' component={Login} />
-        <Route exact path='/post' component={Post} />
-        <Route exact path='/addpost' component={CreatePost} />
-        <Route exact path="/profile" component={CreateProfile} />
-     <Route exact path="/forgotPassword" component={ForgotPassword} />
-        <p>Facebook Authentication</p>
-        <Facebook />
-
-        <Footer />
-      </div>
-    </Router>
-  );
-}
-
-export default App;
->>>>>>> 0a33fcf6c146e209e79408020445ed1b069e7589
