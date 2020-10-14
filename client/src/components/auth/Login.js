dfb8b1443ce1152512192d3616fcd73169dea885
KLishi <67844338+KLishi@users.noreply.github.com>
2020-10-13 20:37:58 -0700

    login to create profile redirection

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -38,13 +38,13 @@ class Login extends Component {
 
   componentDidMount() {
     if (this.props.auth.isAuthenticated) {
-      this.props.history.push('/dashboard');
+      this.props.history.push('/CreateProfile');
     }
   }
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.auth.isAuthenticated) {
-      this.props.history.push('/dashboard');
+      this.props.history.push('/CreateProfile');
     }
     if (nextProps.errors) {
       this.setState({ errors: nextProps.errors });
