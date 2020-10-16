6698ed44a582a047e6d5e840b13361b20d415e6b
Teal Larson <larson.teal@gmail.com>
2020-10-16 13:07:36 -0700

    connected widget to upload

--- client/src/components/createpost/CreatePost.js
+++ client/src/components/createpost/CreatePost.js
@@ -1,41 +1,71 @@
 import React, { Component } from 'react';
 import {Image, CloudinaryContext} from 'cloudinary-react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { addPost } from '../../actions/postActions';
+import {useDropzone} from 'react-dropzone'
+
 
 class CreatePost extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     //Local state
     this.state = {
       imageURL: '',
       caption: '',
       errors: {},
-    }
+    };
 
     this.onChange = this.onChange.bind(this);
-
+    this.onSubmit = this.onSubmit.bind(this);
+      
     this.widget = window.cloudinary.createUploadWidget({
       cloudName: 'socialconnexion',
-      uploadPreset: 'default',
+      uploadPreset: 'st8gcaqq',
+      uploadSignature: this.state.user,
       theme: 'minimal',
       inlineContainer: document.getElementById('upload-widget')
-
     }, (error, result) => {
-      if (!error && result && result.event === "success"){console.log('Done!  Here is the image info:', result.info)}
+      if (!error && result && result.event === "success"){console.log(
+        result.info.secure_url)}
+
     }
     )
   }
 
- 
+  componentWillReceiveProps(newProps) {
+    if(newProps.errors) {
+      this.setState({ errors: newProps.errors });
+    }
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const { user } = this.props.auth;
+
+    const newPost = {
+      caption: this.state.caption,
+      imageURL: this.state.imageURL,
+      handle: user.handle,
+      avatar: user.avatar,
+      user: user
+    };
+
+    this.props.addPost(newPost);
+    this.setState({imageURL: '', caption: ''});
+  }
 
   onChange(e) {
     this.setState({ [e.target.name]: e.target.value });
   };
 
   
-
-
   render() {
+
+    const { errors } = this.state;
+
     return (
       <div className='container col-lg-6 col-md-9 col-sm-9'>
         
@@ -87,12 +117,20 @@ class CreatePost extends Component {
       
       </div>
 
-              
-
-
     );
   }
 }
 
+CreatePost.propTypes = {
+  addPost: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+}
 
-export default CreatePost;
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
+
+
+export default connect(mapStateToProps, { addPost })(CreatePost);
