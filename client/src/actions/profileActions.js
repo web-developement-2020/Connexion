40ef172543d04ee69e9c27e98d5932d7dc1bfe5f
Kakkoo <58194538+Kakkoo@users.noreply.github.com>
2020-10-16 09:09:56 -0700

    g

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -84,62 +84,48 @@ export const getProfiles = () => (dispatch) => {
       dispatch({
         type: GET_PROFILES,
         payload: null,
-
-export const getProfiles = () => dispatch => {
-  dispatch(setProfileLoading());
-  axios
-    .get('/api/profile/all')
-    .then(res =>
-      dispatch({
-        type: GET_PROFILES,
-        payload: res.data
-      })
-    )
-    .catch(err =>
-      dispatch({
-        type: GET_PROFILES,
-        payload: null
-
       })
     );
 };
 
+
+
 // Delete account & profile
 
-export const deleteAccount = () => (dispatch) => {
-  if (window.confirm("Are you sure? This can NOT be undone!")) {
-    axios
-      .delete("/api/profile")
-      .then((res) =>
-        dispatch({
-          type: SET_CURRENT_USER,
-          payload: {},
-        })
-      )
-      .catch((err) =>
-        dispatch({
-          type: GET_ERRORS,
-          payload: err.response.data,
+// export const deleteAccount = () => (dispatch) => {
+//   if (window.confirm("Are you sure? This can NOT be undone!")) {
+//     axios
+//       .delete("/api/profile")
+//       .then((res) =>
+//         dispatch({
+//           type: SET_CURRENT_USER,
+//           payload: {},
+//         })
+//       )
+//       .catch((err) =>
+//         dispatch({
+//           type: GET_ERRORS,
+//           payload: err.response.data,
 
-        })
-      );
-  }
-};
+//         })
+//       );
+//   }
+// };
 
-// Profile loading
-export const setProfileLoading = () => {
-  return {
+// // Profile loading
+// export const setProfileLoading = () => {
+//   return {
 
-    type: PROFILE_LOADING,
+//     type: PROFILE_LOADING,
 
-  };
-};
+//   };
+// };
 
-// Clear profile
-export const clearCurrentProfile = () => {
-  return {
+// // Clear profile
+// export const clearCurrentProfile = () => {
+//   return {
 
-    type: CLEAR_CURRENT_PROFILE,
+//     type: CLEAR_CURRENT_PROFILE,
 
-  };
-};
+//   };
+// };
