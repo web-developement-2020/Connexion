f50002a233639a8511d9e358b339a123546a1fc4
KLishi <67844338+KLishi@users.noreply.github.com>
2020-10-21 14:55:44 -0700

    name change!

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -31,9 +31,9 @@ class Profiles extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-12">
-              <h1 className="display-4 text-center">Developer Profiles</h1>
+              <h1 className="display-4 text-center">Connexion Profiles</h1>
               <p className="lead text-center">
-                Browse and connect with developers
+                Browse and connect!
               </p>
               {profileItems}
             </div>
